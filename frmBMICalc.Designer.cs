9b2802269a55b3ceb8b8bf2c3c82dde22361ad6d
artemkukushkin <42660028+artemkukushkin@users.noreply.github.com>
2018-09-24 14:12:11 -0600

    Add files via upload

---
+++ frmBMICalc.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace BMI_Calculator
+{
+    partial class frmBMICalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFeet = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtInches = new System.Windows.Forms.TextBox();
+            this.lblFeet = new System.Windows.Forms.Label();
+            this.lblInches = new System.Windows.Forms.Label();
+            this.txtPounds = new System.Windows.Forms.TextBox();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblPunds = new System.Windows.Forms.Label();
+            this.txtBMI = new System.Windows.Forms.TextBox();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.bxGender = new System.Windows.Forms.GroupBox();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.txtBMIDes = new System.Windows.Forms.TextBox();
+            this.bxGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFeet
+            // 
+            this.txtFeet.Location = new System.Drawing.Point(107, 28);
+            this.txtFeet.Name = "txtFeet";
+            this.txtFeet.Size = new System.Drawing.Size(100, 20);
+            this.txtFeet.TabIndex = 0;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(39, 31);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(57, 16);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Height:";
+            // 
+            // txtInches
+            // 
+            this.txtInches.Location = new System.Drawing.Point(107, 55);
+            this.txtInches.Name = "txtInches";
+            this.txtInches.Size = new System.Drawing.Size(100, 20);
+            this.txtInches.TabIndex = 2;
+            // 
+            // lblFeet
+            // 
+            this.lblFeet.AutoSize = true;
+            this.lblFeet.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeet.Location = new System.Drawing.Point(213, 31);
+            this.lblFeet.Name = "lblFeet";
+            this.lblFeet.Size = new System.Drawing.Size(29, 13);
+            this.lblFeet.TabIndex = 3;
+            this.lblFeet.Text = "feet";
+            // 
+            // lblInches
+            // 
+            this.lblInches.AutoSize = true;
+            this.lblInches.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInches.Location = new System.Drawing.Point(213, 58);
+            this.lblInches.Name = "lblInches";
+            this.lblInches.Size = new System.Drawing.Size(44, 13);
+            this.lblInches.TabIndex = 4;
+            this.lblInches.Text = "inches";
+            // 
+            // txtPounds
+            // 
+            this.txtPounds.Location = new System.Drawing.Point(107, 102);
+            this.txtPounds.Name = "txtPounds";
+            this.txtPounds.Size = new System.Drawing.Size(100, 20);
+            this.txtPounds.TabIndex = 5;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeight.Location = new System.Drawing.Point(39, 105);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(60, 16);
+            this.lblWeight.TabIndex = 6;
+            this.lblWeight.Text = "Weight:";
+            // 
+            // lblPunds
+            // 
+            this.lblPunds.AutoSize = true;
+            this.lblPunds.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPunds.Location = new System.Drawing.Point(213, 105);
+            this.lblPunds.Name = "lblPunds";
+            this.lblPunds.Size = new System.Drawing.Size(48, 13);
+            this.lblPunds.TabIndex = 7;
+            this.lblPunds.Text = "pounds";
+            // 
+            // txtBMI
+            // 
+            this.txtBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBMI.Location = new System.Drawing.Point(107, 178);
+            this.txtBMI.Name = "txtBMI";
+            this.txtBMI.ReadOnly = true;
+            this.txtBMI.Size = new System.Drawing.Size(74, 21);
+            this.txtBMI.TabIndex = 8;
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(39, 181);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(38, 16);
+            this.lblBMI.TabIndex = 9;
+            this.lblBMI.Text = "BMI:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(61, 238);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 25);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReturn.Location = new System.Drawing.Point(226, 238);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(120, 25);
+            this.btnReturn.TabIndex = 11;
+            this.btnReturn.Text = "&Return to Menu";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // bxGender
+            // 
+            this.bxGender.Controls.Add(this.rbtnMale);
+            this.bxGender.Controls.Add(this.rbtnFemale);
+            this.bxGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bxGender.Location = new System.Drawing.Point(280, 28);
+            this.bxGender.Name = "bxGender";
+            this.bxGender.Size = new System.Drawing.Size(93, 94);
+            this.bxGender.TabIndex = 12;
+            this.bxGender.TabStop = false;
+            this.bxGender.Text = "Gender";
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMale.Location = new System.Drawing.Point(15, 53);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(57, 19);
+            this.rbtnMale.TabIndex = 1;
+            this.rbtnMale.Text = "Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Checked = true;
+            this.rbtnFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnFemale.Location = new System.Drawing.Point(15, 26);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(73, 19);
+            this.rbtnFemale.TabIndex = 0;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtBMIDes
+            // 
+            this.txtBMIDes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBMIDes.Location = new System.Drawing.Point(216, 178);
+            this.txtBMIDes.Name = "txtBMIDes";
+            this.txtBMIDes.ReadOnly = true;
+            this.txtBMIDes.Size = new System.Drawing.Size(157, 21);
+            this.txtBMIDes.TabIndex = 13;
+            // 
+            // frmBMICalc
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.CancelButton = this.btnReturn;
+            this.ClientSize = new System.Drawing.Size(411, 294);
+            this.Controls.Add(this.txtBMIDes);
+            this.Controls.Add(this.bxGender);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.txtBMI);
+            this.Controls.Add(this.lblPunds);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.txtPounds);
+            this.Controls.Add(this.lblInches);
+            this.Controls.Add(this.lblFeet);
+            this.Controls.Add(this.txtInches);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtFeet);
+            this.Name = "frmBMICalc";
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.frmBMICalc_Load);
+            this.bxGender.ResumeLayout(false);
+            this.bxGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFeet;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtInches;
+        private System.Windows.Forms.Label lblFeet;
+        private System.Windows.Forms.Label lblInches;
+        private System.Windows.Forms.TextBox txtPounds;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblPunds;
+        private System.Windows.Forms.TextBox txtBMI;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.GroupBox bxGender;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.TextBox txtBMIDes;
+    }
+}
+
---
+++ frmBMICalc.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class frmBMICalc : Form
+    {
+        public frmBMICalc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBMICalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtInches.Text)) //Error checking for blank Inches field
+            {
+                txtInches.Text = "0";
+            }
+            if (string.IsNullOrWhiteSpace(txtFeet.Text)) //Error checking for blank Feet field
+            {
+                MessageBox.Show("You have to be a bit taller to use this form!");
+                txtFeet.Focus();
+                return;
+            }
+        
+            short feet = Convert.ToInt16(txtFeet.Text);   //assigning value types to the text boxes in the form
+            short inches = Convert.ToInt16(txtInches.Text); //trying to select the types that use the least memory space
+            int totalInches = feet * 12 + inches;
+            float weight = Convert.ToSingle(txtPounds.Text);
+            string bmiDesc = "";        
+
+            double bmi = 703 * weight / Convert.ToUInt16(Math.Pow(totalInches, 2)); //BMI formula
+            bmi = Math.Round(bmi, 1); //rounding to 1 decimal
+
+            // the next set of IFs account for wrong values
+
+            if (feet < 3) 
+            {
+                MessageBox.Show("You have to be a bit taller to use this form!");
+                txtFeet.Focus();
+                return;
+            }
+            else if (feet > 8)
+            {
+                MessageBox.Show("Are you really that tall? If so, exit this application and call Guinness World Records immediately!");
+                txtFeet.Focus();
+                return;
+            }
+
+            if (inches < 0)
+            {
+                MessageBox.Show("Please enter a valid number into the 'inches' field." + "\n" + "Valid value: 0 to 11");
+                txtInches.Clear();
+                txtInches.Focus();
+                return;
+            }
+            else if (inches > 11)
+            {
+                MessageBox.Show("Please enter a valid number into the 'inches' field." + "\n" + "Valid value: 0 to 11");
+                txtInches.Clear();
+                txtInches.Focus();
+                return;
+            }
+
+            // BMI scale for Females is different from Males, and the next set of IFs account for that
+
+            if (rbtnFemale.Checked == true) 
+            {
+                if (bmi < 19.1)
+                {
+                    bmiDesc = "Underweight";
+                }
+                else if (bmi >= 19.1 && bmi < 25.8)
+                {
+                    bmiDesc = "Normal";
+                }
+                else if (bmi >= 25.8 && bmi < 27.3)
+                {
+                    bmiDesc = "Marginally Overweight";
+                }
+                else if (bmi >= 27.3 && bmi <= 32.3)
+                {
+                    bmiDesc = "Overweight";
+                }
+                else bmiDesc = "Obese";
+            }
+
+            if (rbtnMale.Checked == true)
+            {
+                if (bmi < 20.7)
+                {
+                    bmiDesc = "Underweight";
+                }
+                else if (bmi >= 20.7 && bmi < 26.4)
+                {
+                    bmiDesc = "Normal";
+                }
+                else if (bmi >= 26.4 && bmi < 27.8)
+                {
+                    bmiDesc = "Marginally Overweight";
+                }
+                else if (bmi >= 27.8 && bmi <= 31.1)
+                {
+                    bmiDesc = "Overweight";
+                }
+                else bmiDesc = "Obese";
+            }
+
+            txtBMI.Text = bmi.ToString(); // output is displayed
+            txtBMIDes.Text = bmiDesc;
+        }
+
+
+    }
+}
